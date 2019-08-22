523dd1a28cd189941a289e8af65b23cd287fd778
noorfathima11 <afshan.shokath@gmail.com>
2019-08-22 15:20:37 +0530

    Using Props

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const person = (props) => {
-  return <p> I'm a {props.name} I am {Math.floor(Math.random() * 30) } years old </p>
+  return <p> I'm a {props.name} I am {props.age} years old </p>
 }
 
 export default person
