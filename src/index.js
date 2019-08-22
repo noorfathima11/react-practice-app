9686692bd60874e08f5c68d8e02011f0897273ac
noorfathima11 <afshan.shokath@gmail.com>
2019-08-22 11:19:25 +0530

    Add component reusability

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-function Person(){
+function Person(props){
   return (
     <div className="person">
-       <h1>Max</h1>
-       <p>Your Age: 26</p>
+       <h1>{props.name}</h1>
+       <p>Your Age: {props.age}</p>
     </div>
   )
 }
 
-ReactDOM.render(<Person/>, document.querySelector('#p1'))
+ReactDOM.render(<Person name="Max" age="28"/>, document.querySelector('#p1'))
+
+ReactDOM.render(<Person name="Noor" age="26"/>, document.querySelector('#p2'))
